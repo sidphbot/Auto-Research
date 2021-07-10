4f44ec85b3d0557034588a8999018bbf888df247
Sidharth Pal <sidharth.pal1992@gmail.com>
2021-07-10 14:38:25 +0200

    initial commit

--- Surveyor.py
+++ Surveyor.py
@@ -100,19 +100,6 @@ class Surveyor:
             self.embedder = SentenceTransformer(embedder_name)
             self.embedder.eval()
             self.embedder.save(models_dir + "/embedder")
-
-
-            self.nlp = spacy.load(nlp_name)
-            config = self.nlp.config
-            bytes_data = self.nlp.to_bytes()
-            joblib.dump(config, models_dir + "nlp_config.dump")
-            joblib.dump(bytes_data, models_dir + "nlp_bytes_data.dump")
-
-            self.similarity_nlp = spacy.load(similarity_nlp_name)
-            config = self.similarity_nlp.config
-            bytes_data = self.similarity_nlp.to_bytes()
-            joblib.dump(config, models_dir + "similarity_nlp_config.dump")
-            joblib.dump(bytes_data, models_dir + "similarity_nlp_bytes_data.dump")
         else:
             self.title_tokenizer = AutoTokenizer.from_pretrained(title_model_name)
             self.title_model = AutoModelForSeq2SeqLM.from_pretrained(models_dir + "/title_model").to(self.torch_device)
@@ -134,21 +121,8 @@ class Surveyor:
             self.embedder = SentenceTransformer(models_dir + "/embedder")
             self.embedder.eval()
 
-
-            config = joblib.load(models_dir + "nlp_config.dump")
-            bytes_data = joblib.load(models_dir + "nlp_bytes_data.dump")
-            lang_cls = spacy.util.get_lang_class(config["nlp"]["lang"])
-            self.nlp = lang_cls.from_config(config)
-            self.nlp.from_bytes(bytes_data)
-
-            similarity_config = joblib.load(models_dir + "similarity_nlp_config.dump")
-            similarity_bytes_data = joblib.load(models_dir + "similarity_nlp_bytes_data.dump")
-            lang_cls = spacy.util.get_lang_class(similarity_config["nlp"]["lang"])
-            self.nlp = lang_cls.from_config(similarity_config)
-            self.nlp.from_bytes(similarity_bytes_data)
-
-
-
+        self.nlp = spacy.load(nlp_name)
+        self.similarity_nlp = spacy.load(similarity_nlp_name)
         self.kw_model = KeyBERT(kw_model_name)
 
         self.define_structure(pdf_dir=pdf_dir, txt_dir=txt_dir, img_dir=img_dir, tab_dir=tab_dir, dump_dir=dump_dir)
