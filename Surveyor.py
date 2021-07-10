8dd8fd64def7df17e797f700db2db179c92754fa
Sidharth Pal <sidharth.pal1992@gmail.com>
2021-07-11 01:51:48 +0200

    initial commit

--- Surveyor.py
+++ Surveyor.py
@@ -1407,6 +1407,8 @@ if __name__ == '__main__':
                         help='tables storage directory - defaults to arxiv_data/tables/')
     parser.add_argument('--dump_dir', metavar='dump_dir', type=str, default=None,
                         help='all_output_dir - defaults to arxiv_dumps/')
+    parser.add_argument('--models_dir', metavar='save_models_dir', type=str, default=None,
+                        help='directory to save models (> 5GB) - defaults to saved_models/')
     parser.add_argument('--title_model_name', metavar='title_model_name', type=str, default=None,
                         help='title model name/tag in hugging-face, defaults to \'Callidior/bert2bert-base-arxiv-titlegen\'')
     parser.add_argument('--ex_summ_model_name', metavar='extractive_summ_model_name', type=str, default=None,
@@ -1421,6 +1423,10 @@ if __name__ == '__main__':
                         help='spacy downstream model(for similarity) name/tag in hugging-face (if changed - needs to be spacy-installed prior), defaults to \'en_core_sci_lg\'')
     parser.add_argument('--kw_model_name', metavar='kw_model_name', type=str, default=None,
                         help='keyword extraction model name/tag in hugging-face, defaults to \'distilbert-base-nli-mean-tokens\'')
+    parser.add_argument('--refresh_models', metavar='refresh_models', type=str, default=None,
+                        help='Refresh model downloads with given names (needs atleast one model name param above), defaults to False')
+    parser.add_argument('--high_gpu', metavar='high_gpu', type=str, default=None,
+                        help='High GPU usage permitted, defaults to False')
 
     args = parser.parse_args()
     surveyor = Surveyor(
@@ -1429,13 +1435,17 @@ if __name__ == '__main__':
         img_dir=args.img_dir,
         tab_dir=args.tab_dir,
         dump_dir=args.dump_dir,
+        models_dir=args.models_dir,
         title_model_name=args.title_model_name,
         ex_summ_model_name=args.ex_summ_model_name,
         ledmodel_name=args.ledmodel_name,
         embedder_name=args.embedder_name,
         nlp_name=args.nlp_name,
         similarity_nlp_name=args.similarity_nlp_name,
-        kw_model_name=args.kw_model_name
+        kw_model_name=args.kw_model_name,
+        refresh_models=args.refresh_models,
+        high_gpu=args.high_gpu
+
     )
 
     surveyor.survey(args.query, max_search=args.max_search, num_papers=args.num_papers,
