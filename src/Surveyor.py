1a7b27d7acaa7ad24c387cc6d7d6cfae9b18d8e8
Sidharth Pal <sidharth.pal1992@gmail.com>
2021-07-11 23:09:44 +0200

    fixed arxiv_public_data import

--- src/Surveyor.py
+++ src/Surveyor.py
@@ -1428,7 +1428,7 @@ class Surveyor:
 
         return os.path.abspath(self.dump_dir + output_zip), os.path.abspath(survey_file)
 
-
+'''
 if __name__ == '__main__':
     import argparse
 
@@ -1493,4 +1493,4 @@ if __name__ == '__main__':
 
     surveyor.survey(args.query, max_search=args.max_search, num_papers=args.num_papers,
                                               debug=False, weigh_authors=False)
-
+'''
